b16087ef3b018f3807c713280373cd2434caefe4
spitliner <73539619+spitliner@users.noreply.github.com>
2022-03-26 19:02:35 +0000

    Basic refactor on Hitori SA

--- Hitori/hitori_sa.py
+++ Hitori/hitori_sa.py
@@ -12,7 +12,6 @@ class Hitori:
         self.dimension = 0
         self.array_grid = []
         self.solution = []
-        self.current_black = []
         self.have_visit = []
         self.dup_pairs = []
         self.dup_lists = {}
@@ -42,7 +41,7 @@ class Hitori:
     def printInitialGrid(self):
         self.__printGrid(self.array_grid)
 
-    def markDubs(self, pos):
+    def __markDubs(self, pos):
         check_arr_hor = np.empty(11, dtype=object)
         check_arr_ver = np.empty(11, dtype=object)
 
@@ -63,7 +62,7 @@ class Hitori:
         dub_arr = []
 
         for i in range(self.dimension):
-            dub_arr += self.markDubs(i)
+            dub_arr += self.__markDubs(i)
 
         dub_list = sum(dub_arr, [])
 
@@ -71,10 +70,8 @@ class Hitori:
 
         return [create_dict(sorted(Counter(dub_list))), dub_list]
 
-    def checkDubs(self, pos):
+    def __checkDubs(self, pos, check_arr_hor, check_arr_ver):
         dub = 0
-        check_arr_hor = np.full(11, 0)
-        check_arr_ver = np.full(11, 0)
 
         for i in range(self.dimension):
             check_arr_hor[self.temp_arr[pos * self.dimension + i]] += 1
@@ -86,70 +83,23 @@ class Hitori:
                 dub += check_arr_hor[i] - 1
             if 1 < check_arr_ver[i]:
                 dub += check_arr_ver[i] - 1
+                
+            check_arr_hor[i] = check_arr_ver[i] = 0    
 
         return dub
 
-    def countTotalDub(self):
+    def __countTotalDub(self):
         dub_arr = 0
+        
+        check_arr_hor = np.full(11, 0)
+        check_arr_ver = np.full(11, 0)
 
         for i in range(self.dimension):
-            dub_arr += self.checkDubs(i)
+            dub_arr += self.__checkDubs(i, check_arr_hor, check_arr_ver)
 
         return np.sum(dub_arr)
 
-    def __travelBlack(self, pos):
-        result = False
-        if self.have_visit[pos]:
-            return result
-
-        edge_check = lambda x: x < self.dimension or x > self.dimension * (
-                self.dimension - 1) or 0 == x % self.dimension or 0 == (x + 1) % self.dimension
-        edge_flag = False
-        add_flag = False
-        if edge_check(pos):
-            edge_flag = True
-
-        wait_line = deque()
-        probe = [0, 0, 0, 0]
-
-        current_check = pos
-        for i in range(pos, len(self.current_black)):
-            if self.have_visit[i]:
-                continue
-            else:
-                wait_line.append(i)
-
-            while len(wait_line):
-                current_check = wait_line.pop()
-                self.have_visit[pos] = True
-                if (edge_flag and edge_check(current_check)) or pos == current_check:
-                    result = True
-                    break
-
-                edge_flag = edge_check(current_check)
-
-                if self.have_visit[current_check]:
-                    result = False
-                    break
-
-                probe[0] = current_check - self.dimension - 1
-                probe[1] = current_check - self.dimension + 1
-                probe[2] = current_check + self.dimension - 1
-                probe[3] = current_check + self.dimension + 1
-                for ii in range(4):
-                    if probe[ii] < self.dimension ** 2 and probe[ii] > 0 and 0 == self.temp_arr[probe[ii]]:
-                        wait_line.append(probe[ii])
-
-        return result
-
     def __checkIsland(self):
-        """
-        self.have_visit = np.full(self.dimension * self.dimension, False)
-
-        for i in self.current_black:
-            if self.__travelBlack(i):
-                return True
-        """
         whiteCell = []
         visited = []
         flag = False
@@ -175,27 +125,24 @@ class Hitori:
                                     if self.temp_arr[ii] != 0:
                                         wait_list.append(ii)
 
-        return not len(visited) == len(whiteCell)
-
-    def isIsland(self):
-        return self.__checkIsland()
+        return len(visited) != len(whiteCell)
 
     def __checkNoContinueBlack(self):
         lenght = len(self.array_grid)
         bound_check = lambda x: x >= 0 and x < lenght
         check_element = lambda x : [x + 1, x - 1, x + self.dimension, x - self.dimension]
         crit = lambda x : bound_check(x) and 0 == self.temp_arr[x]
-        for i in range(len(self.temp_arr)):
+        for i in self.old_dub_lists.keys():
             if 0 == self.temp_arr[i]:
                 if sum(map(crit ,check_element(i))):
                     return True
         return False
 
-    def checkResult(self):
+    def __checkResult(self):
         return not (self.__checkIsland() or self.__checkNoContinueBlack())
 
     # Quick hard-code case detect and solve
-    def initSolver(self, tempHitori):
+    def __initSolver(self, tempHitori):
         if 2 < self.dimension:
             corner_case = [0, self.dimension - 1, len(self.array_grid) - self.dimension,
                            len(self.array_grid) - 1]
@@ -205,24 +152,19 @@ class Hitori:
                     self.dup_lists[1] = 'w'
                     self.dup_lists[self.dimension] = 'w'
                     tempHitori[0] = 0
-                    self.current_black.append(0)
 
             if self.dimension - 1 in self.dup_lists and self.dimension - 2 in self.dup_lists and 2 * self.dimension - 1 in self.dup_lists:
                 if self.array_grid[self.dimension - 1] == self.array_grid[self.dimension - 2] and self.array_grid[self.dimension - 1] == self.array_grid[2 * self.dimension - 1]:
                     self.dup_lists[self.dimension - 1] = 'b'
-                    self.dup_lists[self.dimension - 2] = 'w'
-                    self.dup_lists[2 * self.dimension - 1] = 'w'
+                    self.dup_lists[self.dimension - 2] = self.dup_lists[2 * self.dimension - 1] = 'w'
                     tempHitori[self.dimension - 1] = 0
-                    self.current_black.append(self.dimension - 1)
 
             if corner_case[2] in self.dup_lists and corner_case[2] - self.dimension in self.dup_lists and corner_case[
                 2] + 1 in self.dup_lists:
                 if self.array_grid[corner_case[2]] == self.array_grid[corner_case[2] - self.dimension] and self.array_grid[corner_case[2]] == self.array_grid[corner_case[2] + 1]:
                     self.dup_lists[corner_case[2]] = 'b'
-                    self.dup_lists[corner_case[2] - self.dimension] = 'w'
-                    self.dup_lists[corner_case[2] + 1] = 'w'
+                    self.dup_lists[corner_case[2] - self.dimension] = self.dup_lists[corner_case[2] + 1] = 'w'
                     tempHitori[corner_case[2]] = 0
-                    self.current_black.append(corner_case[2])
 
             if corner_case[3] in self.dup_lists and corner_case[3] - 1 in self.dup_lists and corner_case[
                 3] - self.dimension in self.dup_lists:
@@ -231,7 +173,6 @@ class Hitori:
                     self.dup_lists[corner_case[3] - 1] = 'w'
                     self.dup_lists[corner_case[3] - self.dimension] = 'w'
                     tempHitori[corner_case[3]] = 0
-                    self.current_black.append(corner_case[3])
 
             for i in self.dup_lists.keys():
                 if 'w' == self.dup_lists[i]:
@@ -240,18 +181,14 @@ class Hitori:
                     if self.array_grid[i] == self.array_grid[i + 1] and self.array_grid[i] == self.array_grid[i - 1]:
                         tempHitori[i + 1] = tempHitori[i - 1] = 0
                         self.dup_lists[i + 1] = self.dup_lists[i - 1] = 'b'
-                        self.current_black.append(i + 1)
-                        self.current_black.append(i - 1)
                 elif (i + self.dimension in self.dup_lists) and (i + self.dimension in self.dup_lists):
                     if self.array_grid[i] == self.array_grid[i + self.dimension] and self.array_grid[i] == self.array_grid[i - self.dimension]:
                         tempHitori[i + self.dimension] = tempHitori[i - self.dimension] = 0
                         self.dup_lists[i + self.dimension] = self.dup_lists[i - self.dimension] = 'b'
-                        self.current_black.append(i + self.dimension)
-                        self.current_black.append(i - self.dimension)
 
         return tempHitori
 
-    def madeNewChange(self, old_score, temperature, keys):
+    def __madeNewChange(self, old_score, temperature, keys):
         pos = keys[self.rng.integers(0, len(keys))]
         old_value = self.temp_arr[pos], self.dup_lists[pos]
         result = 0
@@ -265,27 +202,22 @@ class Hitori:
 
         new_score = self.__scoreCalc()
         score_delta = new_score - old_score
-        #if 0 < score_delta:
-        pr = math.exp((-score_delta / temperature))
+        pr = math.exp(( - score_delta / temperature))
         if self.rng.uniform(0, 1, None) < pr:
             result = score_delta
         else:
             self.temp_arr[pos] = old_value[0]
             self.dup_lists[pos] = old_value[1]
 
-        if 'b' == self.dup_lists[pos]:
-            self.current_black.append(pos)
-
         return result
 
     def __scoreCalc(self):
-        return self.countTotalDub()
+        return self.__countTotalDub()
 
     def fillInitState(self):
         for i in self.dup_lists.keys():
             if 'u' == self.dup_lists[i]:
                 if self.rng.uniform(0, 1, None) < 0.5:
-                    self.current_black.append(i)
                     self.dup_lists[i] = 'b'
                     self.temp_arr[i] = 0
                 else:
@@ -298,7 +230,7 @@ class Hitori:
         decrease_factor = .99
         iterations_max = 0
 
-        self.current_black = []
+
         tmp = self.__getDubsDict()
 
         self.dup_pairs = tmp[1]
@@ -306,7 +238,7 @@ class Hitori:
         temperature = len(self.dup_lists) * 100
         
         hold_arr = np.copy(self.array_grid)
-        self.temp_arr = self.initSolver(hold_arr)
+        self.temp_arr = self.__initSolver(hold_arr)
         
         self.old_dub_lists = self.dup_lists
         
@@ -318,7 +250,7 @@ class Hitori:
         old_score = score = self.__scoreCalc()
         
         previous_score = 0
-        is_solve = (not score) and self.checkResult()
+        is_solve = (not score) and self.__checkResult()
         
         result = []
         counting = 0
@@ -328,25 +260,23 @@ class Hitori:
             previous_score = score
             for i in range(iterations_max):
                 counting += 1
-                score += self.madeNewChange(score, temperature, keys)
+                score += self.__madeNewChange(score, temperature, keys)
                 result.append(score)
                 if 0 == score:
-                    if self.checkResult():
+                    if self.__checkResult():
                         is_solve = True
                         break
 
             temperature *= decrease_factor
             if 0 == score:
-                if self.checkResult():
+                if self.__checkResult():
                     is_solve = True
             elif score >= previous_score:
                 stuckCount += 1
                 if stuckCount > 10:
                     temperature += 10
-                    
             else:
                 stuckCount = 0
-            self.current_black = []
         print("--- %s seconds ---" % (time.time() - start_time))
         self.solution = np.copy(self.temp_arr)
         plt.plot(result)
@@ -361,7 +291,7 @@ def main(argv):
         hitori.load(testCase)
         hitori.solve()
         hitori.printSolution()
-
+        print()
 
 if __name__ == '__main__':
     # main(sys.argv[1:])
